927a0c16d994871767af1f842609d988018e312b
Manuel Alzate <maalzates@gmail.com>
2022-07-08 19:29:47 -0500

    Migrations filled with tables

---
+++ database/migrations/2022_07_09_001245_create_countries_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+
+            // Llave foránea de Organization Id.
+            $table->foreignId('organization_id')->constrained();
+            
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+
+            // Llave foránea de Country
+            $table->foreignId('country_id')->constrained();
+
+            $table->string('postal_code');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
